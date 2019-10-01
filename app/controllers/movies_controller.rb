2c368968ceb1927ac1ee92df9678dccc567e1f12
Glen Lauritsen <GDL2021@tamu.edu>
2019-10-01 21:58:22 +0000

    adding in highlighting (originally skipped in part1)

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -35,10 +35,10 @@ class MoviesController < ApplicationController
     
     if params[:order] == 'title'
       @movies = @movies.order('title')
-      @titleHeader = 'hilite'
+      @titleHeader = "hilite"
     elsif params[:order] == 'release_date'
       @movies = @movies.order('release_date')
-      @releaseHeader = 'hilite'
+      @releaseHeader = "hilite"
     end
     
     session[:ratings] = params[:ratings]
